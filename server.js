60ec1f57e1639538f6807ac973c841fa4b57d9b5
pineapple-miriam <2586096762@qq.com>
2026-02-19 20:57:10 +0800

    initial

---
+++ server.js
@@ -0,0 +1,81 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+let rooms = {};
+
+wss.on('connection', (ws) => {
+    console.log('新连接');
+    
+    ws.on('message', (data) => {
+        try {
+            const msg = JSON.parse(data);
+            console.log('收到:', msg.type);
+            
+            if (msg.type === 'create') {
+                let roomId;
+                do {
+                    roomId = Math.random().toString(36).substr(2, 6).toUpperCase();
+                } while (rooms[roomId]);
+                
+                rooms[roomId] = { host: ws, client: null };
+                ws.roomId = roomId;
+                ws.isHost = true;
+                ws.send(JSON.stringify({ type: 'created', roomId }));
+                console.log('创建房间:', roomId);
+            }
+            
+            else if (msg.type === 'join') {
+                const room = rooms[msg.roomId];
+                if (!room) {
+                    ws.send(JSON.stringify({ type: 'error', message: '房间不存在' }));
+                    return;
+                }
+                if (room.client) {
+                    ws.send(JSON.stringify({ type: 'error', message: '房间已满' }));
+                    return;
+                }
+                
+                room.client = ws;
+                ws.roomId = msg.roomId;
+                ws.isHost = false;
+                room.host.send(JSON.stringify({ type: 'joined' }));
+                ws.send(JSON.stringify({ type: 'start', player: 2 }));
+                console.log('加入房间:', msg.roomId);
+            }
+            
+            else if (msg.type === 'move') {
+                const room = rooms[ws.roomId];
+                if (!room) return;
+                const target = ws.isHost ? room.client : room.host;
+                if (target) {
+                    target.send(JSON.stringify({
+                        type: 'move',
+                        x: msg.x,
+                        y: msg.y,
+                        player: msg.player
+                    }));
+                }
+            }
+        } catch (e) {
+            console.error('错误:', e);
+        }
+    });
+    
+    ws.on('close', () => {
+        console.log('断开连接');
+        if (ws.roomId && rooms[ws.roomId]) {
+            const room = rooms[ws.roomId];
+            const other = ws.isHost ? room.client : room.host;
+            if (other) other.send(JSON.stringify({ type: 'disconnected' }));
+            delete rooms[ws.roomId];
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('服务器运行在端口:', PORT);
+});
