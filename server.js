d97038d047722df80bffcdebd3619d09decc5c89
pineapple-miriam <2586096762@qq.com>
2026-02-21 23:32:37 +0800

    Fix port binding for Render

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, '0.0.0.0', () => {
     console.log('服务器运行在端口:', PORT);
 });
